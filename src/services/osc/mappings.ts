3a76e1a3bca12ca70c37b70c1a9944387b3f4f6a
Nairolf138 <71496832+Nairolf138@users.noreply.github.com>
2025-10-19 19:56:09 +0200

    Add Eos group tools with OSC integrations

--- src/services/osc/mappings.ts
+++ src/services/osc/mappings.ts
@@ -5,6 +5,12 @@ export const oscMappings = {
     dmx: '/eos/dmx/level',
     parameter: '/eos/chan/param',
     info: '/eos/get/channels'
+  },
+  groups: {
+    select: '/eos/group/select',
+    level: '/eos/group/level',
+    info: '/eos/group/info',
+    list: '/eos/group/list'
   }
 } as const;
 
